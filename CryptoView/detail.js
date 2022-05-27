8575ce6f92dba3a2b36db4db14f61b27035645a0
Swapnil30901 <swapnilshah30901@gmail.com>
2022-05-27 14:10:30 +0530

    detail screen & search coin is done

--- CryptoView/detail.js
+++ CryptoView/detail.js
@@ -23,48 +23,75 @@ function showPrice(data) {
 
 }
 function showHistory(data) {
-    console.log(data);
+    // console.log(data);
+    showGraph(data);
 }
 
 
 fetch("https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false").then(convertToJson).then(showInfo);
 fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr%2Cusd%2Ceur").then(convertToJson).then(showPrice);
-fetch("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=inr&days=1").then(convertToJson).then(showHistory);
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=inr&days=14&interval=daily").then(convertToJson).then(showHistory);
+
+function convertUnixToReadable(timestamp){ 
+    const date = new Date(timestamp);
+    const date_string = date.getDate();
+    const month_string = "0" + date.getMonth();
+    
+    const readable =  date_string + "-" + month_string;
+    return readable;
+}
+
+function showGraph(history_data){
+    // console.log(history_data.prices);
+    let labels=[];
+    let prices=[];
 
 
+    for(let i=0;i<history_data.prices.length; i += 1){
+        const single_price = history_data.prices[i];
 
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+        const readable_label = convertUnixToReadable(single_price[0]);
+
+        labels.push(readable_label);
+        prices.push(single_price[1]);
+    }
+    console.log(history_data.prices);
+    // console.log(labels);
+    // console.log(prices);
+
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Price (in INR)',
+                data: prices,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
+    });
     }
-});
